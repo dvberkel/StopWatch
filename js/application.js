8a121b3f69016f60925049877d4c980bceb9d5a3
dvberkel <daan.v.berkel.1980@gmail.com>
2015-05-13 10:26:47 +0200

    Create a Game view

--- js/application.js
+++ js/application.js
@@ -3,6 +3,7 @@
     console.log('Ready to play!');
 
     var game = new stopwatch.Game(5);
+    new stopwatch.GameView(game, document.getElementById('stopwatch'));
 
     window.game = game;
 })(stopwatch);
--- js/stopwatch.js
+++ js/stopwatch.js
@@ -2,4 +2,14 @@
     var Game = stopwatch.Game = function(seconds){
         this.target = 1000 * seconds; // in milliseconds
     };
+
+    var GameView = stopwatch.GameView = function(game, container){
+        this.game = game;
+        this.container = container;
+        this.update();
+    };
+    GameView.prototype.update = function(){
+        var target = this.container.querySelector('#target');
+        target.innerHTML = this.game.target;
+    };
 })(window.stopwatch = window.stopwatch || {});
