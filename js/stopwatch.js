fa03a474f29e26078d0a9cd8700eca18c3107e50
dvberkel <daan.v.berkel.1980@gmail.com>
2015-05-13 10:52:57 +0200

    Create a start and stop method

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,10 +1,27 @@
 (function(stopwatch){
     var Game = stopwatch.Game = function(seconds){
         this.target = 1000 * seconds; // in milliseconds
+        this.started = false;
+        this.stopped = false;
     };
     Game.prototype.current = function(){
+        if (this.started) {
+            return (this.stopped ? this.stopTime: this.time) - this.startTime;
+        }
         return 0;
     };
+    Game.prototype.start = function(){
+        if (!this.started) {
+            this.started = true;
+            this.startTime = new Date().getTime();
+            this.time = this.startTime;
+        }
+    };
+    Game.prototype.stop = function(){
+        if (!this.stopped) {
+            this.stopped = true;
+            this.stopTime = new Date().getTime();
+        }
     };
 
     var GameView = stopwatch.GameView = function(game, container){
