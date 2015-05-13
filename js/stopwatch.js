985a68d7761968b0b62d18185fa2328aae309802
dvberkel <daan.v.berkel.1980@gmail.com>
2015-05-13 10:31:11 +0200

    Add current to Game

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -2,6 +2,10 @@
     var Game = stopwatch.Game = function(seconds){
         this.target = 1000 * seconds; // in milliseconds
     };
+    Game.prototype.current = function(){
+        return 0;
+    };
+    };
 
     var GameView = stopwatch.GameView = function(game, container){
         this.game = game;
@@ -11,5 +15,7 @@
     GameView.prototype.update = function(){
         var target = this.container.querySelector('#target');
         target.innerHTML = this.game.target;
+        var current = this.container.querySelector('#current');
+        current.innerHTML = this.game.current();
     };
 })(window.stopwatch = window.stopwatch || {});
