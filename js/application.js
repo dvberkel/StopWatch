37f04f5f5548fc2cbdfdbf5f2ea7b593c5e2643f
dvberkel <daan.v.berkel.1980@gmail.com>
2015-05-13 10:10:25 +0200

    Create a Game model

--- js/application.js
+++ js/application.js
@@ -1,4 +1,8 @@
 (function(stopwatch){
     if (!stopwatch) { throw new Error('stopwatch namespace not found'); }
     console.log('Ready to play!');
+
+    var game = new stopwatch.Game(5);
+
+    window.game = game;
 })(stopwatch);
--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,2 +1,5 @@
 (function(stopwatch){
+    var Game = stopwatch.Game = function(seconds){
+        this.target = 1000 * seconds; // in milliseconds
+    };
 })(window.stopwatch = window.stopwatch || {});
